534b9fc3b1c5599a588d6f4dd70a8f7e6a1a2f17
tasita <uatasita@gmail.com>
2023-04-23 23:32:33 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,41 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const numberInput = document.querySelector('#controls input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesArea = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', getNumberCreateBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  const divArray = [];
+  let defaultSize = 30;
+  
+  for (let i = 0; i <= amount; i += 1) {
+    const divEl = document.createElement("div");
+    divEl.style.width = defaultSize + 'px';
+    divEl.style.height = defaultSize + 'px';
+    divEl.style.backgroundColor = getRandomHexColor();
+    defaultSize += 10;
+    divArray.push(divEl);
+  }
+
+  boxesArea.append(...divArray);
+}
+
+
+function getNumberCreateBoxes(event) {
+  destroyBoxes();
+  createBoxes(numberInput.value);
+  numberInput.value = '';
+}
+
+function destroyBoxes() {
+  boxesArea.innerHTML = '';
+}
+
+
+
